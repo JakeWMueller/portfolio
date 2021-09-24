6f1150d5977275072599699105b75ffa58995ce0
JakeWMueller <jake.william.mueller@gmail.com>
2021-09-24 11:40:31 -0500

    Background iframe YT component

--- src/components/hi/Hi.jsx
+++ src/components/hi/Hi.jsx
@@ -1,9 +1,10 @@
+import Winter from "../youtube/winter";
 import "./hi.scss";
 
 export default function Hi() {
     return (
         <div className="hi" id="hi">
-            
+            <Winter/>
         </div>
     )
-}
+    };
