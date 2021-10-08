85ec7184cb720e0ed8bac7070195adfdf45c5b50
JakeWMueller <jake.william.mueller@gmail.com>
2021-10-07 23:52:00 -0500

    Writing page and Medium gif.

--- src/components/writing/Writing.jsx
+++ src/components/writing/Writing.jsx
@@ -1,16 +1,19 @@
 import "./writing.scss";
+import mediumGif from "../../assets/medium.gif";
 
 export default function Writing() {
     return (
         <div className="writing" id="writing" style={{
             display: "flex",
             position: "relative",
-            backgroundColor: "whitesmoke",
+            backgroundColor: "white",
             backgroundSize: "cover",
-            width: "100vw",
+            width: "100%",
             height: "100%",
         }}>
-            
+            <div className="medium" id="medium">
+                <a href="https://medium.com/@jake.william.mueller" target="_blank" rel="noreferrer"><img src={mediumGif} alt="mediumGif"></img></a>
+            </div>
         </div>
     )
 }
