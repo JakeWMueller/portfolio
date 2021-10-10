5003c31a99ccef9aa365a3e922118857ff736783
JakeWMueller <jake.william.mueller@gmail.com>
2021-10-10 15:28:31 -0500

    It is finished

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -3,13 +3,21 @@ import { Mail, Instagram, LinkedIn, GitHub } from "@material-ui/icons";
 import { useState } from "react";
 
 export default function Contact() {
-
+    const changePosition = () => {
+        (document.getElementById('Bn').style.bottom='500px')
+    };
     const [ message, setMessage ] = useState(false);
 
     const handleSubmit = (e)=> {
         e.preventDefault();
         setMessage(true);
-    }
+    };
+    const [ welldone, setWellDone ] = useState(false);
+
+    const handleYes = (e)=> {
+        e.preventDefault();
+        setWellDone(true);
+    };
     return (
         <div className="contact" id="contact" style={{
             display: "flex",
@@ -38,11 +46,50 @@ export default function Contact() {
                             <button type="submit" className="btn btn-lg btn-dark btn-block">Submit Form</button>
                             {message && <span>Great! I'll get back to you</span>}
                     </form>
-                    <div className="bottom">
+                <div className="bottom">
                     <a href="https://www.linkedin.com/in/jake-mueller" target="_blank" rel="noreferrer"><LinkedIn/></a>
                     <a href="https://www.github.com/JakeWMueller" target="_blank" rel="noreferrer"><GitHub/></a>
                     <a href="https://www.instagram.com/jakeneedsfriends/" target="_blank" rel="noreferrer"><Instagram/></a>
                 </div>
+                <div style={{
+                    display: "flex",
+                    position: "relative"
+                }}>
+                        <h1 style={{
+                                display: "flex",
+                                position: "relative",
+                                width: "auto",
+                            }}
+                        >
+                            Hire me?
+                        </h1>
+                </div>
+                <div className="veryBottom" style={{
+                    display: "flex",
+                    position: "relative",
+                    alignContent: "center",
+                    justifyContent: "center",
+                    padding: "5px"
+                }}>
+                        <div id="By" style={{
+                                width: "40%",
+                                height: "30%",
+                                padding: "5px"
+                            }}
+                        >
+                            <input type="button" defaultValue=" yes " onClick={handleYes}/>
+                        </div>
+                        <div id="Bn" style={{
+                                position: "relative",
+                                width: "30%",
+                                height: "30%",
+                                padding: "5px"
+                            }}
+                        >
+                            <input type="button" id="Bn" className="no" defaultValue=" no " onMouseMove={changePosition}/>
+                        </div>
+                </div>
+                {welldone && <span>Good thinking! Fill out the contact form.</span>}
             </div>
         </div>
         
